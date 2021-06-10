1bf40006365aaa38b878e0e5606eb5be32547c28
001ZNC744 <001ZNC744@LAPTOP-LEESJVQH>
2021-06-10 12:11:24 +0530

    Updated Today's Project

--- 10_06_21/src/comm/example/EmployeeType.java
+++ 10_06_21/src/comm/example/EmployeeType.java
@@ -2,7 +2,15 @@ package comm.example;
 
 public enum EmployeeType {
 	
-	SALARIED,
-	HOORLY_PAID
-
+	SALARIED("Salaried Employee"),
+	HOORLY_PAID("Hourly paid Employee");
+	
+	private String message;
+	private EmployeeType(String message)
+	{
+		this.message=message;
+	}
+   public String getMessage() {
+	   return message;
+   }
 }
