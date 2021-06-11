5f1601fb82af92f7fa13e6d70d50c14653a87769
001ZNC744 <001ZNC744@LAPTOP-LEESJVQH>
2021-06-11 12:05:34 +0530

    updated exception

--- Exceptions/src/com/example/test/Test.java
+++ Exceptions/src/com/example/test/Test.java
@@ -1,21 +1,23 @@
 package com.example.test;
 import java.util.InputMismatchException;
+import com.example.exception.Employee2;
+import com.example.exception.Manager2;
 import java.util.Scanner;
 import java.util.UUID;
 import com.example.exception.ExceptionT;
 public class Test {
 	private static Scanner sc=new Scanner(System.in);
     
-	private ExceptionT[] exception;
+	/*private ExceptionT[] exception;
 	
 	{
 		exception=new ExceptionT[5];
-	}
+	}*/
 	
 	public static void main(String args[])
 	{
 		Test t=new Test();
-		for(int i=0;i<5;i++)
+		/*for(int i=0;i<5;i++)
 		{
 			try {
 				String tempId =UUID.randomUUID().toString();
@@ -40,7 +42,33 @@ public class Test {
 			
 		}
 	
-    }
+    }*/
+		Employee2 emp = null;
+		Manager2 man = new Manager2();
+		t.check(man);
+
+		t.check(emp);
+	}
+
+	private void check(Employee2 emp) {
+		try {
+			if (emp instanceof Manager2) {
+				Manager2 manager = (Manager2) emp;
+				System.out.println("Manager" + manager.toString());
+			} else {
+				System.out.println(emp.toString());
+			}
+
+		
+		}catch (ArrayIndexOutOfBoundsException e) {
+			System.err.println("array index out of bound exception");
+		}
+		catch (ArithmeticException e) {
+			System.err.println("arithemetic");
+			e.printStackTrace();
+		}
+		
+	}
 }
 
 
