650b6ac721de2856f370464957b733eed8ae3537
Harika7799 <85544000+Harika7799@users.noreply.github.com>
2021-06-30 16:56:29 +0530

    Add files via upload

---
+++ Java_Script_Node/var.js
@@ -0,0 +1,134 @@
+/*let var1 = 9;
+let var2 = 9;
+//Equal ===
+console.log(var1 === var2)
+//NotEqual !=
+console.log(var1 != var2)
+//Greater than
+console.log(var1 > var2)
+//Less than
+console.log(var1 < var2)
+//Greater than equal to
+console.log(var1 >= var2)
+//Less than equal to
+console.log(var1 <= var2)
+
+//let num1 = 100;
+//let num2 = 1000;
+ 
+//let result = (num1 < num2) ? "yay" : "nay";
+ 
+//console.log(result);
+
+//IF_ELSE
+
+let num1 = 314;
+let num2 = 314;
+if(num1<num2)
+{
+    console.log('Num1 is less than Num2');
+}
+else if (num1>num2)
+{
+    console.log('Num1 is greater than Num2');
+}
+else {
+    console.log('Num1 is Equal to Num2');
+}
+
+//======================== FOR =======================
+
+for (var i = 1000; i <= 100; i += 2)
+{
+    console.log("This is position for I: " + i);
+}
+ //============================ WHILE ====================
+var j = 1000;
+ 
+while (j <= 100)
+{
+    console.log("This is position for J: " + j);
+    j += 2;
+}
+ 
+var k = 1000;
+ 
+do
+{
+    console.log("This is position for K: " + k);
+    k += 2;
+} while (k <= 100)
+let awesomeVar = 0;
+
+
+//==========================SWITCH========================
+
+switch (awesomeVar)
+{
+    case 0:
+    console.log("This is 0");
+    break;
+ 
+    case 1:
+    console.log("This is 1");
+    break;
+ 
+    case 2:
+    console.log("This is 2");
+    break;
+ 
+    case 3:
+    console.log("This is 3");
+    break;
+ 
+    case 4:
+    console.log("This is 4");
+    break;
+ 
+    case 5:
+    console.log("This is 5");
+    break;
+ 
+    default:
+    console.log("This is the default value");
+    break;
+}*/
+//=========================FUNCTION======================
+function _myFunction()
+{
+    let a=100;
+    let b=200;
+    return('addition of a and b is: ',(a+b));
+}
+ 
+let result=_myFunction();
+console.log(result);
+ 
+function AwesomeFunc()
+{
+    console.log("Hello");
+    console.log("and");
+    console.log("Goodbye");
+}
+ 
+function Sum(num1, num2)
+{
+    let result = num1 + num2;
+ 
+    console.log(result);
+}
+ 
+function SumReturn(num1, num2)
+{
+    let result = num1 + num2;
+ 
+    return result;
+}
+ 
+AwesomeFunc();
+Sum(4, 7);
+console.log(SumReturn(5, 9));
+ 
+let epicVar = SumReturn(2, 100);
+ 
+console.log(epicVar);
