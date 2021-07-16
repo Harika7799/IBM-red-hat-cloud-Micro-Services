4c05fdf383a3616e3c40736099ce936c2b1ddfb9
Harika7799 <85544000+Harika7799@users.noreply.github.com>
2021-07-16 11:59:26 +0530

    SERVERS

--- adim-server-project/src/main/java/com/example/demo/config/SecurityConfig.java
+++ adim-server-project/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.demo.config;
+/*package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,4 +16,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		super.configure(http);
 	}
 
-}
+}*/
