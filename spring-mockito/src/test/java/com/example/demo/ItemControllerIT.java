7f5bf4fb95ddcf33e6c5927c413589cf73826cf9
001ZNC744 <addalaharika9009@gmail.com>
2021-07-30 14:26:35 +0530

    mockito

---
+++ spring-mockito/src/test/java/com/example/demo/ItemControllerIT.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ItemControllerIT {
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
+	@Test
+	public void exampleTest2() {
+		String body = this.restTemplate.getForObject("/items", String.class);
+		assertThat(body).contains("Pencil");
+	}
+
+}
