83619e46baf6eaf3bdb0635155c8c48e977ad0f8
Harika7799 <85544000+Harika7799@users.noreply.github.com>
2021-08-02 11:30:27 +0530

    Add files via upload

---
+++ my-app/src/index.js
@@ -0,0 +1,8 @@
+
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+// eslint-disable-next-line no-unused-vars
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(<App />, document.getElementById('root'));
